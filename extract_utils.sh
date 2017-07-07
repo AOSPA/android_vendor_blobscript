bac8fbfe7c258c90fcac5e3e2003e1ec4764a5bf
Willi Ye <williye97@gmail.com>
2017-07-07 23:12:39 +0200

    Force moving xml file
    * On UNIX it will ask you everytime
    
    Change-Id: I5c9a80f688598b876f82e67e751de090a58043d8

--- extract_utils.sh
+++ extract_utils.sh
@@ -754,7 +754,7 @@ function fix_xml() {
     grep -a '^<?xml version' "$XML" > "$TEMP_XML"
     grep -av '^<?xml version' "$XML" >> "$TEMP_XML"
 
-    mv "$TEMP_XML" "$XML"
+    mv -f "$TEMP_XML" "$XML"
 }
 
 #
