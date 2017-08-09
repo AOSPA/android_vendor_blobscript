edae4ea8a6bcf3eddbbf547c4bf16af6dbea03c7
Alex Naidis <alex.naidis@linux.com>
2017-08-09 17:49:11 +0200

    blobscript: Add support for vendor/framework rules
    Some vendors add java libraries to vendor/framework.
    
    This patch adds support to properly parse these and
    create packaging rules for them.
    
    Change-Id: I9689aa613254823cb92fc46f38eb137d4e884103
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- extract_utils.sh
+++ extract_utils.sh
@@ -386,6 +386,10 @@ function write_product_packages() {
     if [ "${#FRAMEWORK[@]}" -gt "0" ]; then
         write_packages "JAVA_LIBRARIES" "false" "" "FRAMEWORK" >> "$ANDROIDMK"
     fi
+    local V_FRAMEWORK=( $(prefix_match "vendor/framework/") )
+    if [ "${#V_FRAMEWORK[@]}" -gt "0" ]; then
+        write_packages "JAVA_LIBRARIES" "true" "" "V_FRAMEWORK" >> "$ANDROIDMK"
+    fi
 
     # Etc
     local ETC=( $(prefix_match "etc/") )
